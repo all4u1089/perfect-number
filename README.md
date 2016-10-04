2145ed6454ef878084e8a61c5541ac3e172df21d
Pham Xuan Thanh <Pham Xuan Thanh>
2016-10-05 00:44:04 +0700

    add CI

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
+[![Build Status](https://travis-ci.org/all4u1089/perfect-number.svg?branch=master)](https://travis-ci.org/all4u1089/perfect-number)
+[![Coverage Status](https://coveralls.io/repos/github/all4u1089/perfect-number/badge.svg)](https://coveralls.io/github/all4u1089/perfect-number)
+
 ## Installation
-  
+
 ## Usage
 
 ## Tests
--- test/index.js
+++ test/index.js
@@ -1,45 +1,45 @@
 var expect = require('chai').expect;
 var check = require('../src/index.js');
 
-describe('Check perfect number test case', function () {
+describe('Check perfect number test case', function() {
 
-	it('returns error when value is not numeric', function (done) {
-		check.isPerfectNumber('x', function (err,s) {
-			expect(err).to.not.equal(null);
-			expect(err.message).to.equal('it must be a numeric');
-			done();
-		});
-	});
+  it('returns error when value is not numeric', function(done) {
+    check.isPerfectNumber('x', function(err, s) {
+      expect(err).to.not.equal(null);
+      expect(err.message).to.equal('it must be a numeric');
+      done();
+    });
+  });
 
-	it('returns error when value is not positive', function (done) {
-		check.isPerfectNumber(-6, function (err,s) {
-			expect(err).to.not.equal(null);
-			expect(err.message).to.equal('number must be positive');
-			done();
-		});
-	});
+  it('returns error when value is not positive', function(done) {
+    check.isPerfectNumber(-6, function(err, s) {
+      expect(err).to.not.equal(null);
+      expect(err.message).to.equal('number must be positive');
+      done();
+    });
+  });
 
-	it('returns true when value is 6', function (done) {
-		check.isPerfectNumber(6, function (err, s) {
-			expect(err).to.equal(null);
-			expect(s).to.equal(true);
-			done();
-		});
-	});
+  it('returns true when value is 6', function(done) {
+    check.isPerfectNumber(6, function(err, s) {
+      expect(err).to.equal(null);
+      expect(s).to.equal(true);
+      done();
+    });
+  });
 
-	it('returns false when value is 7', function (done) {
-		check.isPerfectNumber(7, function (err, s) {
-			expect(err).to.equal(null);
-			expect(s).to.equal(false);
-			done();
-		});
-	});
+  it('returns false when value is 7', function(done) {
+    check.isPerfectNumber(7, function(err, s) {
+      expect(err).to.equal(null);
+      expect(s).to.equal(false);
+      done();
+    });
+  });
 
-	it('returns true when value is 28', function (done) {
-		check.isPerfectNumber(28, function (err, s) {
-			expect(err).to.equal(null);
-			expect(s).to.equal(true);
-			done();
-		});
-	});
+  it('returns true when value is 28', function(done) {
+    check.isPerfectNumber(28, function(err, s) {
+      expect(err).to.equal(null);
+      expect(s).to.equal(true);
+      done();
+    });
+  });
 });
