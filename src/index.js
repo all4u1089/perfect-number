83bbf2d6d74e39d364f97543f1fcb36402e5bbcb
Pham Xuan Thanh <Pham Xuan Thanh>
2016-10-05 00:08:30 +0700

    add jsbeautify

--- src/index.js
+++ src/index.js
@@ -1,24 +1,23 @@
 var isPerfectNumber = function isPerfectNumber(number, callback) {
 
-	if (isNaN(number)) {
-		return callback(new Error('it must be a numeric'));
-	}
+  if (isNaN(number)) {
+    return callback(new Error('it must be a numeric'));
+  }
+  if (number < 0) {
+    return callback(new Error('number must be positive'));
+  }
 
-	if (number < 0) {
-		return callback(new Error('number must be positive'));
-	}
+  var sum = 0;
 
-	var sum = 0;
-
-	for (var i = number - 1; i > 0; i--) {
-		if (number % i === 0) {
-			sum += i;
-		}
-	}
-	var result = sum === number;
-	callback(null, result);
+  for (var i = number - 1; i > 0; i--) {
+    if (number % i === 0) {
+      sum += i;
+    }
+  }
+  var result = sum === number;
+  return callback(null, result);
 };
 
 module.exports = {
-	isPerfectNumber: isPerfectNumber
+  isPerfectNumber
 };
